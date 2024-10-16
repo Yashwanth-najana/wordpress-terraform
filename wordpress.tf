ec46b82f2fc94601f29456738dabc1a3c750c051
Yashwanth Najana <yaswanthnajana@gmail.com>
2024-10-16 22:58:57 +0530

    wordpress.tf

--- wordpress.tf
+++ wordpress.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
 resource "aws_instance" "wordpress" {
   ami                         = "ami-00f251754ac5da7f0"
   instance_type               = "t2.medium"
