35ed38a46074b83536103a659543577dccadc883
Yashwanth Najana <yaswanthnajana@gmail.com>
2024-10-16 19:54:47 +0530

    wordpress.tf

--- wordpress.tf
+++ wordpress.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = " "
-  secret_key = " "
+  access_key = "AKIAZI2LEBIH77DCMTNO"
+  secret_key = "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"
 }
 
 resource "aws_instance" "instance" {
